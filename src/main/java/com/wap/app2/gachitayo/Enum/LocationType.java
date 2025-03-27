551435a43dad6bb111163cb1afee2a3fffbc6212
squareCaaat <danny79052@gmail.com>
2025-03-27 13:34:37 +0900

    refactor: 변경 LocationType에 JsonCreator 어노테이션 추가 #4

--- src/main/java/com/wap/app2/gachitayo/Enum/LocationType.java
+++ src/main/java/com/wap/app2/gachitayo/Enum/LocationType.java
@@ -1,5 +1,17 @@
 package com.wap.app2.gachitayo.Enum;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum LocationType {
-    START, STOPOVER, DESTINATION
+    START, STOPOVER, DESTINATION;
+
+    @JsonCreator
+    public static LocationType fromString(String locationType) {
+        for(LocationType type : LocationType.values()) {
+            if(type.name().equalsIgnoreCase(locationType)) {
+                return type;
+            }
+        }
+        return null;
+    }
 }
