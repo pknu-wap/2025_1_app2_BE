409f71a15e24b10a962fcdf68a6e54f999e584d2
squareCaaat <danny79052@gmail.com>
2025-03-28 00:22:57 +0900

    feat: 추가 StopoverUpdateDto #4

---
+++ src/main/java/com/wap/app2/gachitayo/dto/request/StopoverUpdateDto.java
@@ -0,0 +1,11 @@
+package com.wap.app2.gachitayo.dto.request;
+
+import com.wap.app2.gachitayo.dto.datadto.StopoverDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StopoverUpdateDto {
+    StopoverDto stopover;
+}
