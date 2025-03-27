42573129c416f8bd38361599fd37191994d13e00
squareCaaat <danny79052@gmail.com>
2025-03-27 11:33:52 +0900

    refactor: 변경 파티 엔티티 속성 Column 명시, Stopover 속성 추가 #2

--- src/main/java/com/wap/app2/gachitayo/domain/party/Party.java
+++ src/main/java/com/wap/app2/gachitayo/domain/party/Party.java
@@ -1,8 +1,7 @@
 package com.wap.app2.gachitayo.domain.party;
 
 import com.wap.app2.gachitayo.Enum.GenderOption;
-import com.wap.app2.gachitayo.domain.location.Destination;
-import com.wap.app2.gachitayo.domain.location.Location;
+import com.wap.app2.gachitayo.domain.location.Stopover;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -13,15 +12,15 @@ public class Party {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, name="max_person")
     private Integer maxParticipants;
+
+    @Column(nullable = false, name="radius")
+    private double allowRadius = 1.0;
 
     @Enumerated(EnumType.STRING)
     private GenderOption genderOption;
 
-    @OneToOne
-    @JoinColumn(name = "location_id")
-    private Location startLocation;
-
     @OneToMany(mappedBy = "party", cascade = CascadeType.ALL)
-    private List<Destination> destinations = new ArrayList<>();
+    private List<Stopover> stopovers = new ArrayList<>();
 }
