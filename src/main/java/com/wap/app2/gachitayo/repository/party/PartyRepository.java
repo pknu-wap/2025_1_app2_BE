f529c9eda5265d4e757cf9c6f9d8a0c642366e5f
squareCaaat <danny79052@gmail.com>
2025-03-28 00:26:14 +0900

    feat: 추가 PartyRepository #4

---
+++ src/main/java/com/wap/app2/gachitayo/repository/party/PartyRepository.java
@@ -0,0 +1,7 @@
+package com.wap.app2.gachitayo.repository.party;
+
+import com.wap.app2.gachitayo.domain.party.Party;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyRepository extends JpaRepository<Party, Long> {
+}
