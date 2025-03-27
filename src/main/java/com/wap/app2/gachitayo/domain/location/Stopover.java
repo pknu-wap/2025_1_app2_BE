9be0773d9b9f16b7416a3cec329425134e512ed1
squareCaaat <danny79052@gmail.com>
2025-03-27 13:33:28 +0900

    refactor: 변경 Stopover에 생성자 추가, 빌더 패턴 적용 #4

--- src/main/java/com/wap/app2/gachitayo/domain/location/Stopover.java
+++ src/main/java/com/wap/app2/gachitayo/domain/location/Stopover.java
@@ -3,7 +3,11 @@ package com.wap.app2.gachitayo.domain.location;
 import com.wap.app2.gachitayo.Enum.LocationType;
 import com.wap.app2.gachitayo.domain.party.Party;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Setter;
 
+@Builder(access = AccessLevel.PROTECTED)
 @Entity
 public class Stopover {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,10 +16,22 @@ public class Stopover {
     @OneToOne(fetch = FetchType.LAZY)
     private Location location;
 
+    @Setter(AccessLevel.PROTECTED)
     @ManyToOne
     @JoinColumn(name = "party_id")
     private Party party;
 
+    @Setter(AccessLevel.PROTECTED)
     @Enumerated(EnumType.STRING)
     private LocationType stopoverType;
+
+    public Stopover() {}
+
+    public Stopover(Location location, Party party, LocationType stopoverType) {
+        this.location = location;
+        this.party = party;
+        this.stopoverType = stopoverType;
+    }
+
 }
+
