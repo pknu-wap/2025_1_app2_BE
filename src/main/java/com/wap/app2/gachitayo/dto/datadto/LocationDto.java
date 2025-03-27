5f6cf531719d0c60efba5283c7df3a38e8d16e03
squareCaaat <danny79052@gmail.com>
2025-03-28 00:11:14 +0900

    refactor: 변경 LocationDto 구조 #4

--- src/main/java/com/wap/app2/gachitayo/dto/datadto/LocationDto.java
+++ src/main/java/com/wap/app2/gachitayo/dto/datadto/LocationDto.java
@@ -1,16 +1,15 @@
 package com.wap.app2.gachitayo.dto.datadto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.wap.app2.gachitayo.Enum.LocationType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LocationDto {
     private String name;
     @JsonProperty("lat")
     private Double latitude;
     @JsonProperty("lng")
     private Double longitude;
-    @JsonProperty("location_type")
-    private LocationType locationType;
 }
