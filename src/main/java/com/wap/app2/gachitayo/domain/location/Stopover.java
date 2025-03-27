9f74610a3c6542184ad1d327091da5e398f435d5
squareCaaat <danny79052@gmail.com>
2025-03-28 00:13:04 +0900

    refactor: 변경 Stopover 생성자 Lombok 처리, access 수준 재설정 #4

--- src/main/java/com/wap/app2/gachitayo/domain/location/Stopover.java
+++ src/main/java/com/wap/app2/gachitayo/domain/location/Stopover.java
@@ -3,12 +3,14 @@ package com.wap.app2.gachitayo.domain.location;
 import com.wap.app2.gachitayo.Enum.LocationType;
 import com.wap.app2.gachitayo.domain.party.Party;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Setter;
+import lombok.*;
 
-@Builder(access = AccessLevel.PROTECTED)
+
+@Builder
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Stopover {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,22 +18,13 @@ public class Stopover {
     @OneToOne(fetch = FetchType.LAZY)
     private Location location;
 
-    @Setter(AccessLevel.PROTECTED)
+    @Setter
     @ManyToOne
     @JoinColumn(name = "party_id")
     private Party party;
 
-    @Setter(AccessLevel.PROTECTED)
+    @Setter
     @Enumerated(EnumType.STRING)
     private LocationType stopoverType;
-
-    public Stopover() {}
-
-    public Stopover(Location location, Party party, LocationType stopoverType) {
-        this.location = location;
-        this.party = party;
-        this.stopoverType = stopoverType;
-    }
-
 }
 
