d315295787c2e67da58adf24e589b2c96f40b087
squareCaaat <danny79052@gmail.com>
2025-03-27 11:31:49 +0900

    refactor: 변경 위치 정보 엔티티 속성 이름 #2

--- src/main/java/com/wap/app2/gachitayo/domain/location/Location.java
+++ src/main/java/com/wap/app2/gachitayo/domain/location/Location.java
@@ -1,17 +1,18 @@
 package com.wap.app2.gachitayo.domain.location;
 
+import com.wap.app2.gachitayo.Enum.LocationType;
 import jakarta.persistence.*;
 
 @Entity
 public class Location {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+
+    private String locationName;
 
     @Column(nullable = false)
     private double latitude;
 
     @Column(nullable = false)
     private double longitude;
-
 }
