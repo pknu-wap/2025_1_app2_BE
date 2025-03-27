4f146a03bb758e29decf3fa915ee601a5d66b3b4
squareCaaat <danny79052@gmail.com>
2025-03-28 00:19:06 +0900

    refactor: 변경 PartyCreateRequestDto StopoverDto 객체로 변경 #4

--- src/main/java/com/wap/app2/gachitayo/dto/request/PartyCreateRequestDto.java
+++ src/main/java/com/wap/app2/gachitayo/dto/request/PartyCreateRequestDto.java
@@ -2,15 +2,17 @@ package com.wap.app2.gachitayo.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.wap.app2.gachitayo.Enum.GenderOption;
-import com.wap.app2.gachitayo.dto.datadto.LocationDto;
+import com.wap.app2.gachitayo.dto.datadto.StopoverDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PartyCreateRequestDto {
     @JsonProperty("party_start")
-    private LocationDto startLocation;
+    private StopoverDto startLocation;
     @JsonProperty("party_destination")
-    private LocationDto destination;
+    private StopoverDto destination;
     @JsonProperty("party_radius")
     private Double radius;
     @JsonProperty("party_max_person")
