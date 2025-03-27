5ededc5f4a3e049f598ec371273e1ab240a6b665
squareCaaat <danny79052@gmail.com>
2025-03-27 13:33:06 +0900

    refactor: 변경 Party에 생성자 추가, 빌더 패턴 적용 #4

--- src/main/java/com/wap/app2/gachitayo/domain/party/Party.java
+++ src/main/java/com/wap/app2/gachitayo/domain/party/Party.java
@@ -3,19 +3,24 @@ package com.wap.app2.gachitayo.domain.party;
 import com.wap.app2.gachitayo.Enum.GenderOption;
 import com.wap.app2.gachitayo.domain.location.Stopover;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Builder(access = AccessLevel.PROTECTED)
 @Entity
 public class Party {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, name="max_person")
-    private Integer maxParticipants;
+    @Builder.Default
+    private Integer maxPerson = 3;
 
     @Column(nullable = false, name="radius")
+    @Builder.Default
     private double allowRadius = 1.0;
 
     @Enumerated(EnumType.STRING)
@@ -23,4 +28,13 @@ public class Party {
 
     @OneToMany(mappedBy = "party", cascade = CascadeType.ALL)
     private List<Stopover> stopovers = new ArrayList<>();
+
+    public Party() {}
+
+    public Party(Integer maxPerson, Double allowRadius, GenderOption genderOption, Stopover stopover) {
+        this.maxPerson = maxPerson;
+        this.allowRadius = allowRadius;
+        this.genderOption = genderOption;
+        this.stopovers.add(stopover);
+    }
 }
