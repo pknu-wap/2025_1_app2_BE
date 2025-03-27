c6db9ebb6d2393f5351a857a1dcaadb47dde673f
squareCaaat <danny79052@gmail.com>
2025-03-27 13:32:23 +0900

    refactor: 변경 Location에 Stopover 관계 설정 #4

--- src/main/java/com/wap/app2/gachitayo/domain/location/Location.java
+++ src/main/java/com/wap/app2/gachitayo/domain/location/Location.java
@@ -1,8 +1,11 @@
 package com.wap.app2.gachitayo.domain.location;
 
-import com.wap.app2.gachitayo.Enum.LocationType;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Setter;
 
+@Builder(access = AccessLevel.PROTECTED)
 @Entity
 public class Location {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +18,16 @@ public class Location {
 
     @Column(nullable = false)
     private double longitude;
+
+    @Setter(AccessLevel.PROTECTED)
+    @OneToOne(fetch = FetchType.LAZY)
+    private Stopover stopover;
+
+    public Location() {}
+
+    public Location(String locationName, double latitude, double longitude) {
+        this.locationName = locationName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
