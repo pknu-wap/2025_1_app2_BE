b7af81aae5386d4330a3107f8e2ab8f60251a6f1
squareCaaat <danny79052@gmail.com>
2025-03-30 20:05:02 +0900

    feat: findByLocationAndStopoverType 추가
    Stopover 중복 저장을 막기 위한 조회 Operation 추가
    #4

--- src/main/java/com/wap/app2/gachitayo/repository/location/StopoverRepository.java
+++ src/main/java/com/wap/app2/gachitayo/repository/location/StopoverRepository.java
@@ -1,8 +1,13 @@
 package com.wap.app2.gachitayo.repository.location;
 
+import com.wap.app2.gachitayo.Enum.LocationType;
+import com.wap.app2.gachitayo.domain.location.Location;
 import com.wap.app2.gachitayo.domain.location.Stopover;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface StopoverRepository extends JpaRepository<Stopover, Long> {
+    Optional<Stopover> findByLocationAndStopoverType(Location location, LocationType stopoverType);
 }
