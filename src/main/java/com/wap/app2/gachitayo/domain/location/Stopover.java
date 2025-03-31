d3120b55480a97a66061e0498d5216424f89099d
squareCaaat <danny79052@gmail.com>
2025-03-31 11:31:09 +0900

    feat: Stopover update 메서드 추가
    - 기존 객체나 받은 객체가 null 일 수도 있어서 `Objects.equals()` 사용
    - location이나 stopoverType 중 하나라도 업데이트될 경우 업데이트 사항을 알리기 위해 boolean 반환
    #7

--- src/main/java/com/wap/app2/gachitayo/domain/location/Stopover.java
+++ src/main/java/com/wap/app2/gachitayo/domain/location/Stopover.java
@@ -5,6 +5,8 @@ import com.wap.app2.gachitayo.domain.party.Party;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Objects;
+
 
 @Builder
 @Entity
@@ -26,5 +28,18 @@ public class Stopover {
     @Setter
     @Enumerated(EnumType.STRING)
     private LocationType stopoverType;
+
+    public boolean update(Location location, LocationType stopoverType) {
+        boolean isUpdated = false;
+        if(!Objects.equals(this.location, location)) {
+            this.location = location;
+            isUpdated = true;
+        }
+        if(!Objects.equals(this.stopoverType, stopoverType)) {
+            this.stopoverType = stopoverType;
+            isUpdated = true;
+        }
+        return isUpdated;
+    }
 }
 
