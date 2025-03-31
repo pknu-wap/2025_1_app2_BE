4fc3eaf8af11762bec2a3e198d2d8aedeb68b5d4
squareCaaat <danny79052@gmail.com>
2025-03-31 23:08:46 +0900

    remove: StopoverResponseDto 삭제
    - StopoverDto을 응답으로 사용하기에 삭제
    Ref: #7
    Related to: #8

--- src/main/java/com/wap/app2/gachitayo/dto/response/StopoverResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.wap.app2.gachitayo.dto.response;
-
-import com.wap.app2.gachitayo.dto.datadto.StopoverDto;
-
-public record StopoverResponseDto(
-        Long partyId,
-        StopoverDto stopover
-) {
-}
