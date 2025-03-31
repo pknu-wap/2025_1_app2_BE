4b4b11680d3d9ff3be04ff7f122d87b974530658
squareCaaat <danny79052@gmail.com>
2025-03-31 23:01:44 +0900

    refactor: Stopover id 추가 및 유연성
    - id 필드 추가 (응답, 업데이트용) 생성 시 사용 X
    - `@JsonIgnoreProperties` 추가로 정의되지 않은 필드는 무시 따라서 요청과 응답에 유연성 있게 적용 가능
    Ref: #7
    Related to: #8

--- src/main/java/com/wap/app2/gachitayo/dto/datadto/StopoverDto.java
+++ src/main/java/com/wap/app2/gachitayo/dto/datadto/StopoverDto.java
@@ -1,6 +1,7 @@
 package com.wap.app2.gachitayo.dto.datadto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.wap.app2.gachitayo.Enum.LocationType;
 import com.wap.app2.gachitayo.Enum.validator.ValidEnum;
@@ -10,7 +11,10 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StopoverDto {
+    private Long id;
     private LocationDto location;
     @JsonProperty("stopover_type")
     @ValidEnum(enumClass= LocationType.class, message = "Invalid stopover type")
