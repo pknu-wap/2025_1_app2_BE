7f4da90920d9dc50fbbb54f5b9bbbfa2aec76801
Lepitar <leekus1006@gmail.com>
2025-04-04 14:52:43 +0900

    feat: 응답 dto 추가

--- src/main/java/com/wap/app2/gachitayo/controller/auth/GoogleAuthController.java
+++ src/main/java/com/wap/app2/gachitayo/controller/auth/GoogleAuthController.java
@@ -1,11 +1,14 @@
 package com.wap.app2.gachitayo.controller.auth;
 
+import com.wap.app2.gachitayo.dto.request.LoginRequestDto;
+import com.wap.app2.gachitayo.dto.request.RegisterRequestDto;
 import com.wap.app2.gachitayo.dto.response.TokenResponseDto;
 import com.wap.app2.gachitayo.service.auth.GoogleAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +20,12 @@ public class GoogleAuthController {
     private final GoogleAuthService googleAuthService;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponseDto> userLogin() {
-        return googleAuthService.getUserDetail();
+    public ResponseEntity<TokenResponseDto> userLogin(@RequestBody LoginRequestDto requestDto) {
+        return googleAuthService.getUserDetail(requestDto);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> userRegister() {
-        return googleAuthService.getUserDetail();
+    public ResponseEntity<TokenResponseDto> userRegister(@RequestBody RegisterRequestDto requestDto) {
+        return googleAuthService.registerUser(requestDto);
     }
 }
