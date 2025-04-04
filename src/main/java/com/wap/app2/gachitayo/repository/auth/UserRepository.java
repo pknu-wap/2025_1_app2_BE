4096face578d7d3190ca85d545f7f39ddc37dcda
Lepitar <leekus1006@gmail.com>
2025-04-04 14:51:41 +0900

    feat: findByEmail 추가

--- src/main/java/com/wap/app2/gachitayo/repository/auth/UserRepository.java
+++ src/main/java/com/wap/app2/gachitayo/repository/auth/UserRepository.java
@@ -2,6 +2,8 @@ package com.wap.app2.gachitayo.repository.auth;
 
 import com.wap.app2.gachitayo.domain.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
