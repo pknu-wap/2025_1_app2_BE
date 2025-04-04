fba43ebb597d167cd5c61ca2f4eb8c59896d9848
Lepitar <leekus1006@gmail.com>
2025-04-04 14:52:55 +0900

    feat: 유저 저장 추가

--- src/main/java/com/wap/app2/gachitayo/service/auth/GoogleAuthService.java
+++ src/main/java/com/wap/app2/gachitayo/service/auth/GoogleAuthService.java
@@ -1,8 +1,12 @@
 package com.wap.app2.gachitayo.service.auth;
 
+import com.wap.app2.gachitayo.domain.User.User;
 import com.wap.app2.gachitayo.domain.auth.Token;
+import com.wap.app2.gachitayo.dto.request.LoginRequestDto;
+import com.wap.app2.gachitayo.dto.request.RegisterRequestDto;
 import com.wap.app2.gachitayo.dto.response.TokenResponseDto;
 import com.wap.app2.gachitayo.jwt.JWTUtil;
+import com.wap.app2.gachitayo.repository.auth.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,8 +15,28 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GoogleAuthService {
     private final JWTUtil jwtUtil;
+    private final UserRepository userRepository;
 
-    public ResponseEntity<TokenResponseDto> getUserDetail() {
+    public ResponseEntity<TokenResponseDto> getUserDetail(LoginRequestDto requestDto) {
         return ResponseEntity.ok(TokenResponseDto.from(new Token(jwtUtil.createToken("email"), jwtUtil.createToken("test"))));
+    }
+
+    public ResponseEntity<TokenResponseDto> registerUser(RegisterRequestDto requestDto) {
+        User user = User.builder()
+                .name(requestDto.name())
+                .phone(requestDto.phone())
+                .age(requestDto.age())
+                .email("test@gmail.com")
+                .profileImageUrl(requestDto.profileImageUrl())
+                .build();
+
+        ResponseEntity<TokenResponseDto> test = getUserDetail(new LoginRequestDto(requestDto.idToken(), requestDto.accessToken()));
+
+        userRepository.save(user);
+        return test;
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElse(null);
     }
 }
