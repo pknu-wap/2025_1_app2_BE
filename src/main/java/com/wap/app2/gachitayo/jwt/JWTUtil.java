fbcd23bce879479e166b5d68b052d9823601946b
Lepitar <leekus1006@gmail.com>
2025-04-07 21:43:00 +0900

    feat: createToken, isExpired 추가

--- src/main/java/com/wap/app2/gachitayo/jwt/JWTUtil.java
+++ src/main/java/com/wap/app2/gachitayo/jwt/JWTUtil.java
@@ -14,14 +14,26 @@ public class JWTUtil {
     private final SecretKey secretKey;
 
     public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
-        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), ((SecretKey) Jwts.SIG.HS256.key().build()).getAlgorithm());
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
-    public Boolean isExpired() {
-        return true;
+    public Boolean isExpired(String token) {
+        Date expiration = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+
+        return expiration.before(new Date()); // 만료일이 지금보다 전이면 만료됨
     }
 
-    public String createToken(String email) {
-        return Jwts.builder().claim("email", email).issuedAt(new Date(System.currentTimeMillis())).expiration(new Date(System.currentTimeMillis() + 12312313)).signWith(this.secretKey).compact();
+    public String createToken(String email, Long expiredMs) {
+        return Jwts.builder()
+            .claim("email", email)
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + expiredMs))
+            .signWith(this.secretKey)
+            .compact();
     }
 }
