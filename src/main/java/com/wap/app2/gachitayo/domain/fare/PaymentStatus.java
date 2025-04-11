e4f6b0969fed83c156f852b4497cce9d0d6cb507
squareCaaat <danny79052@gmail.com>
2025-04-11 17:29:54 +0900

    feat: PaymentStatus 엔티티 추가

---
+++ src/main/java/com/wap/app2/gachitayo/domain/fare/PaymentStatus.java
@@ -0,0 +1,33 @@
+package com.wap.app2.gachitayo.domain.fare;
+
+import com.wap.app2.gachitayo.domain.location.Stopover;
+import com.wap.app2.gachitayo.domain.party.PartyMember;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment_status")
+public class PaymentStatus {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "party_member_id")
+    private PartyMember partyMember;
+
+    @NotNull
+    @ManyToOne
+    @Setter
+    @JoinColumn(name = "stopover_id")
+    private Stopover stopover;
+
+    @NotNull
+    @Builder.Default
+    boolean isPaid = false;
+}
