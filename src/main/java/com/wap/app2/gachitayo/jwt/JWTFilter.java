7ea7c14e502df424bbb6ea915585c374248efb38
Lepitar <leekus1006@gmail.com>
2025-04-11 16:31:48 +0900

    feat: jwt 필터 추가

---
+++ src/main/java/com/wap/app2/gachitayo/jwt/JWTFilter.java
@@ -0,0 +1,60 @@
+package com.wap.app2.gachitayo.jwt;
+
+import com.wap.app2.gachitayo.domain.User.User;
+import com.wap.app2.gachitayo.domain.User.UserMemberDetails;
+import com.wap.app2.gachitayo.repository.auth.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+@Slf4j
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Bearer")) {
+            log.info("Token Null");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorization.split(" ")[1];
+
+        if (!jwtTokenProvider.isValid(token)) {
+            log.info("Token not valid");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String email = jwtTokenProvider.getEmailByToken(token);
+
+        User user = userRepository.findByEmail(email).orElse(null);
+
+        if (user == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        UserMemberDetails userMemberDetails = new UserMemberDetails(user);
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userMemberDetails, null, new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        filterChain.doFilter(request, response);
+    }
+}
