49524db2cd711a7b696630531f504bae6673eb73
squareCaaat <danny79052@gmail.com>
2025-04-12 01:23:30 +0900

    feat: `connectMemberWithParty` 메서드 추가
    - Party-Member 잇는 PartyMember 생성
    #24

--- src/main/java/com/wap/app2/gachitayo/service/party/PartyMemberService.java
+++ src/main/java/com/wap/app2/gachitayo/service/party/PartyMemberService.java
@@ -1,12 +1,25 @@
 package com.wap.app2.gachitayo.service.party;
 
+import com.wap.app2.gachitayo.Enum.PartyMemberRole;
+import com.wap.app2.gachitayo.domain.Member.Member;
+import com.wap.app2.gachitayo.domain.party.Party;
+import com.wap.app2.gachitayo.domain.party.PartyMember;
 import com.wap.app2.gachitayo.repository.party.PartyMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PartyMemberService {
     private final PartyMemberRepository partyMemberRepository;
 
+    @Transactional
+    public PartyMember connectMemberWithParty(Party party, Member member, PartyMemberRole role) {
+        return partyMemberRepository.save(PartyMember.builder()
+                .party(party)
+                .member(member)
+                .memberRole(role)
+                .build());
+    }
 }
