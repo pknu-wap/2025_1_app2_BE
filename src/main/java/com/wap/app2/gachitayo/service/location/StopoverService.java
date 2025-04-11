c8319169991bb61cdf0c15b8134a4c8b9c2db5be
squareCaaat <danny79052@gmail.com>
2025-04-11 17:55:52 +0900

    refactor: StopoverService 변경
    Fare 연결 구현
    update 시 T/F 반환
    Location 기준으로 동일한지 비교 메서드 구현

--- src/main/java/com/wap/app2/gachitayo/service/location/StopoverService.java
+++ src/main/java/com/wap/app2/gachitayo/service/location/StopoverService.java
@@ -1,13 +1,14 @@
 package com.wap.app2.gachitayo.service.location;
 
 import com.wap.app2.gachitayo.Enum.LocationType;
+import com.wap.app2.gachitayo.domain.fare.Fare;
+import com.wap.app2.gachitayo.domain.fare.PaymentStatus;
 import com.wap.app2.gachitayo.domain.location.Location;
 import com.wap.app2.gachitayo.domain.location.Stopover;
-import com.wap.app2.gachitayo.domain.party.Party;
 import com.wap.app2.gachitayo.dto.datadto.LocationDto;
 import com.wap.app2.gachitayo.repository.location.StopoverRepository;
+import com.wap.app2.gachitayo.service.fare.FareService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,33 +18,42 @@ import org.springframework.transaction.annotation.Transactional;
 public class StopoverService {
     private final StopoverRepository stopoverRepository;
     private final LocationService locationService;
+    private final FareService fareService;
 
     @Transactional
     public Stopover createStopover(LocationDto locationDto, LocationType stopoverType) {
         Location locationEntity = locationService.createOrGetLocation(locationDto);
-        Stopover newStopover = stopoverRepository
-                .save(Stopover.builder()
-                    .location(locationEntity)
-                    .stopoverType(stopoverType)
-                    .build());
+        Stopover newStopover = Stopover.builder()
+                .location(locationEntity)
+                .stopoverType(stopoverType)
+                .build();
+
         locationService.updateStopover(newStopover.getLocation(), newStopover);
-        return newStopover;
+        Fare fare = fareService.createDefaultFare(newStopover);
+        newStopover.setFare(fare);
+        return stopoverRepository.save(newStopover);
     }
 
     @Transactional
-    public void setStopoverToParty(Stopover stopover, Party party) {
-        stopover.setParty(party);
+    public void setPaymentStatus(Stopover stopover, PaymentStatus paymentStatus) {
+        stopover.getPaymentStatusList().add(paymentStatus);
         stopoverRepository.save(stopover);
     }
 
+    public boolean existWithLocation(Stopover stopover, Location location) {
+        return stopover.getLocation().getAddress().equalsIgnoreCase(location.getAddress())
+                && stopover.getLocation().getLatitude() == location.getLatitude()
+                && stopover.getLocation().getLongitude() == location.getLongitude();
+    }
+
     @Transactional
-    public ResponseEntity<?> updateStopover(Stopover stopover, LocationDto locationDto, LocationType stopoverType) {
+    public boolean updateStopover(Stopover stopover, LocationDto locationDto, LocationType stopoverType) {
         Location locationEntity = (locationDto != null) ? locationService.createOrGetLocation(locationDto) : null;
         if(!stopover.update(locationEntity, stopoverType)) {
-            return ResponseEntity.ok().body("no changed");
+            return false;
         }
 
         stopoverRepository.save(stopover);
-        return ResponseEntity.ok().body("updated stopover");
+        return true;
     }
 }
