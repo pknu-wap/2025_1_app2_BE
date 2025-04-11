1cc112ca4c911394523122d796abb5ac0365846f
squareCaaat <danny79052@gmail.com>
2025-04-11 17:37:11 +0900

    refactor: Party 각 필드 수정
    - 제약 조건 및 기본값 설정
    - PartyMember 추가

--- src/main/java/com/wap/app2/gachitayo/domain/party/Party.java
+++ src/main/java/com/wap/app2/gachitayo/domain/party/Party.java
@@ -3,11 +3,13 @@ package com.wap.app2.gachitayo.domain.party;
 import com.wap.app2.gachitayo.Enum.GenderOption;
 import com.wap.app2.gachitayo.domain.location.Stopover;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,18 +22,31 @@ public class Party {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, name="max_person")
+    @NotNull
+    @Column(name="max_person")
     @Builder.Default
-    private Integer maxPerson = 3;
+    private Integer maxPeople = 3;
 
-    @Column(nullable = false, name="radius")
+    @NotNull
+    @Column(name="radius")
     @Builder.Default
-    private double allowRadius = 1.0;
+    private double allowRadius = 0.0; // m 단위, 0 이면 같은 목적지만
 
+    @NotNull
+    @Column(name="departure_time")
+    @Builder.Default
+    private LocalDateTime departureTime = LocalDateTime.now();
+
+    @NotNull
     @Enumerated(EnumType.STRING)
-    private GenderOption genderOption;
+    @Builder.Default
+    private GenderOption genderOption = GenderOption.MIXED;
 
-    @OneToMany(mappedBy = "party", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "party", cascade = CascadeType.REMOVE, orphanRemoval = true)
     @Builder.Default
     private List<Stopover> stopovers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "party", cascade = CascadeType.REMOVE)
+    @Builder.Default
+    private List<PartyMember> partyMembers = new ArrayList<>();
 }
