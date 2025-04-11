2c10f222a63b542df9231db51c5e1e12c7ac37b0
squareCaaat <danny79052@gmail.com>
2025-04-11 17:40:32 +0900

    rename: StopoverDto ValidEnum 이름 변경
    ValidLocationType 으로 변경

--- src/main/java/com/wap/app2/gachitayo/dto/datadto/StopoverDto.java
+++ src/main/java/com/wap/app2/gachitayo/dto/datadto/StopoverDto.java
@@ -3,7 +3,7 @@ package com.wap.app2.gachitayo.dto.datadto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.wap.app2.gachitayo.Enum.LocationType;
-import com.wap.app2.gachitayo.Enum.validator.ValidEnum;
+import com.wap.app2.gachitayo.Enum.validator.ValidLocationType;
 import lombok.*;
 
 @Getter
@@ -16,6 +16,6 @@ public class StopoverDto {
     private Long id;
     private LocationDto location;
     @JsonProperty("stopover_type")
-    @ValidEnum(enumClass= LocationType.class, message = "Invalid stopover type")
+    @ValidLocationType(enumClass= LocationType.class, message = "Invalid stopover type")
     private LocationType stopoverType;
 }
