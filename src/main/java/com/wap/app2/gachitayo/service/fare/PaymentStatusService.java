3d1d3bbd266b3213bc14a491ff6859df25ba5ebd
squareCaaat <danny79052@gmail.com>
2025-04-11 23:11:44 +0900

    feat: connectPartyMemberWithStopover 추가
    - PartyMember-Stopover 잇는 PaymentStatus 엔티티 생성
    Resolves: #23

--- src/main/java/com/wap/app2/gachitayo/service/fare/PaymentStatusService.java
+++ src/main/java/com/wap/app2/gachitayo/service/fare/PaymentStatusService.java
@@ -1,13 +1,26 @@
 package com.wap.app2.gachitayo.service.fare;
 
+import com.wap.app2.gachitayo.domain.fare.PaymentStatus;
+import com.wap.app2.gachitayo.domain.location.Stopover;
+import com.wap.app2.gachitayo.domain.party.PartyMember;
 import com.wap.app2.gachitayo.repository.fare.PaymentStatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentStatusService {
     private final PaymentStatusRepository paymentStatusRepository;
+
+    @Transactional
+    public PaymentStatus connectPartyMemberWithStopover(PartyMember partyMember, Stopover stopover) {
+        PaymentStatus paymentStatus = PaymentStatus.builder()
+                .partyMember(partyMember)
+                .stopover(stopover)
+                .build();
+        return paymentStatusRepository.save(paymentStatus);
+    }
 
 
 }
