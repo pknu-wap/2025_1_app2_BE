0dadf528a752e6e1f599ad5b8cb3508d3d6eb599
Lepitar <leekus1006@gmail.com>
2025-04-11 16:32:28 +0900

    feat: 회원가입 요청 성별 추가

--- src/main/java/com/wap/app2/gachitayo/dto/request/RegisterRequestDto.java
+++ src/main/java/com/wap/app2/gachitayo/dto/request/RegisterRequestDto.java
@@ -1,9 +1,12 @@
 package com.wap.app2.gachitayo.dto.request;
 
+import com.wap.app2.gachitayo.Enum.Gender;
+
 public record RegisterRequestDto(
         String name,
         String phone,
         int age,
+        Gender gender,
         String profileImageUrl,
         String idToken,
         String accessToken
