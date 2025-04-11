606f1b60ee5162c87fa45b1b2d03b23bdc998f1f
squareCaaat <danny79052@gmail.com>
2025-04-11 18:00:21 +0900

    refactor: PartyController 메서드 수정
    요청 Dto 변경
    Header 로부터 id 받음

--- src/main/java/com/wap/app2/gachitayo/controller/party/PartyController.java
+++ src/main/java/com/wap/app2/gachitayo/controller/party/PartyController.java
@@ -1,8 +1,9 @@
 package com.wap.app2.gachitayo.controller.party;
 
-import com.wap.app2.gachitayo.dto.datadto.StopoverDto;
 import com.wap.app2.gachitayo.dto.request.PartyCreateRequestDto;
 import com.wap.app2.gachitayo.dto.request.PartySearchRequestDto;
+import com.wap.app2.gachitayo.dto.request.StopoverAddRequestDto;
+import com.wap.app2.gachitayo.dto.request.StopoverUpdateDto;
 import com.wap.app2.gachitayo.dto.response.PartyCreateResponseDto;
 import com.wap.app2.gachitayo.service.party.PartyService;
 import lombok.RequiredArgsConstructor;
@@ -16,8 +17,8 @@ public class PartyController {
     private final PartyService partyService;
 
     @PostMapping
-    public ResponseEntity<PartyCreateResponseDto> createParty(@RequestBody PartyCreateRequestDto requestDto) {
-        return partyService.createParty(requestDto);
+    public ResponseEntity<PartyCreateResponseDto> createParty(@RequestHeader("member_id") Long memberId, @RequestBody PartyCreateRequestDto requestDto) {
+        return partyService.createParty(memberId, requestDto);
     }
 
     @GetMapping("/{id}")
@@ -26,17 +27,27 @@ public class PartyController {
     }
 
     @PostMapping("/{id}")
-    public ResponseEntity<?> addStopoverToParty(@PathVariable("id") Long id, @RequestBody StopoverDto stopoverDto) {
-        return partyService.addStopoverToParty(id, stopoverDto);
+    public ResponseEntity<?> addStopoverToParty(@PathVariable("id") Long id, @RequestHeader("host_id") Long hostId, @RequestBody StopoverAddRequestDto requestDto) {
+        return partyService.addStopoverToParty(id, hostId, requestDto);
+    }
+
+    @PutMapping("/{id}/attend")
+    public ResponseEntity<?> attendToParty(@RequestHeader("member_id") Long memberId, @PathVariable("id") Long partyId) {
+        return partyService.attendParty(memberId, partyId);
     }
 
     @PostMapping("/search")
-    public ResponseEntity<?> searchPartyWithLocation(@RequestBody PartySearchRequestDto requestDto) {
-        return partyService.searchPartyWithDestinationLocation(requestDto);
+    public ResponseEntity<?> searchPartyWithLocation(@RequestHeader("member_id") Long memberId, @RequestBody PartySearchRequestDto requestDto) {
+        return partyService.searchPartyWithDestinationLocation(memberId, requestDto);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<?> updateStopover(@PathVariable("id") Long id, @RequestBody StopoverDto stopoverDto) {
-        return partyService.updateStopover(id, stopoverDto);
+    public ResponseEntity<?> updateStopover(@PathVariable("id") Long id, @RequestBody StopoverUpdateDto requestDto) {
+        return partyService.updateStopover(id, requestDto);
+    }
+
+    @GetMapping("/{id}/fare")
+    public ResponseEntity<?> getPartyPaymentStatus(@PathVariable("id") Long id) {
+        return partyService.getPaymentStatusAdvanced(id);
     }
 }
