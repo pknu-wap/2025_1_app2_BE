7076a570d5f281873a75c7da3851624722f40d44
squareCaaat <danny79052@gmail.com>
2025-04-28 20:48:14 +0900

    feature: PartyService `findOrCreateStopover` 추가
    - 새로 추가할 Stopover가 파티 내에 있는지 검증하는 것을 PartyService 단으로 옮김
    - Party의 Stopover를 사용하기에 PartyService가 담당하게 됨
    - 파티 내 없는 Stopover라면 새로 생성해서 추가
    #45

--- src/main/java/com/wap/app2/gachitayo/service/party/PartyService.java
+++ src/main/java/com/wap/app2/gachitayo/service/party/PartyService.java
@@ -1,11 +1,16 @@
 package com.wap.app2.gachitayo.service.party;
 
 import com.wap.app2.gachitayo.Enum.GenderOption;
+import com.wap.app2.gachitayo.Enum.LocationType;
+import com.wap.app2.gachitayo.domain.location.Location;
 import com.wap.app2.gachitayo.domain.location.Stopover;
 import com.wap.app2.gachitayo.domain.party.Party;
+import com.wap.app2.gachitayo.dto.datadto.LocationDto;
 import com.wap.app2.gachitayo.error.exception.ErrorCode;
 import com.wap.app2.gachitayo.error.exception.TagogayoException;
+import com.wap.app2.gachitayo.mapper.LocationMapper;
 import com.wap.app2.gachitayo.repository.party.PartyRepository;
+import com.wap.app2.gachitayo.service.location.StopoverService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +22,8 @@ import java.util.List;
 @Transactional
 public class PartyService {
     private final PartyRepository partyRepository;
+    private final LocationMapper locationMapper;
+    private final StopoverService stopoverService;
 
     public Party createParty(List<Stopover> stopovers, Integer maxPeople, Double radius, GenderOption genderOption) {
         Party party = Party.builder()
@@ -31,4 +38,20 @@ public class PartyService {
     public Party findPartyById(Long id) {
         return partyRepository.findById(id).orElseThrow(() -> new TagogayoException(ErrorCode.PARTY_NOT_FOUND));
     }
+
+    @Transactional
+    public Stopover findOrCreateStopover(Party party, LocationDto locationDto) {
+        Location location = locationMapper.toEntity(locationDto);
+
+        return party.getStopovers().stream()
+                .filter(stopover -> stopover.getLocation().isSameLocation(location))
+                .findFirst()
+                .orElseGet(() -> {
+                    Stopover newStopover = stopoverService.createStopover(locationDto, LocationType.STOPOVER);
+                    newStopover.setParty(party);
+                    party.getStopovers().add(newStopover);
+                    return newStopover;
+                });
+    }
+
 }
