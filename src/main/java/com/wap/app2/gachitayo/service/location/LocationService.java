55998d71f27df49fc98dd619a11784613445a07d
squareCaaat <danny79052@gmail.com>
2025-04-29 22:38:40 +0900

    refactor: LocationService 간소화
    - 단순 CRUD 수행
    #44

--- src/main/java/com/wap/app2/gachitayo/service/location/LocationService.java
+++ src/main/java/com/wap/app2/gachitayo/service/location/LocationService.java
@@ -1,7 +1,6 @@
 package com.wap.app2.gachitayo.service.location;
 
 import com.wap.app2.gachitayo.domain.location.Location;
-import com.wap.app2.gachitayo.domain.location.Stopover;
 import com.wap.app2.gachitayo.dto.datadto.LocationDto;
 import com.wap.app2.gachitayo.mapper.LocationMapper;
 import com.wap.app2.gachitayo.repository.location.LocationRepository;
@@ -10,20 +9,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class LocationService {
     private final LocationRepository locationRepository;
     private final LocationMapper locationMapper;
 
+    @Transactional(readOnly = true)
     public Location createOrGetLocation(LocationDto locationDto) {
         return locationRepository.findByLatitudeAndLongitude(locationDto.getLatitude(), locationDto.getLongitude())
                 .map(existingLocation -> locationMapper.toEntityWithExisting(locationDto, existingLocation))
                 .orElseGet(() -> locationRepository.save(locationMapper.toEntity(locationDto)));
-    }
-
-    @Transactional
-    public void updateStopover(Location location, Stopover stopover) {
-        location.getStopovers().add(stopover);
-        locationRepository.save(location);
     }
 }
