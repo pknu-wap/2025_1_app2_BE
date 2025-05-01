a241a644ee1ed50d9568293bcd2bdb47095bcda0
squareCaaat <danny79052@gmail.com>
2025-05-01 17:41:14 +0900

    feat: PartyRepository `deleteExpiredParties` 추가
    - 만료 시간 기준으로 파티 삭제 로직 추가
    #57

--- src/main/java/com/wap/app2/gachitayo/repository/party/PartyRepository.java
+++ src/main/java/com/wap/app2/gachitayo/repository/party/PartyRepository.java
@@ -1,10 +1,13 @@
 package com.wap.app2.gachitayo.repository.party;
 
 import com.wap.app2.gachitayo.domain.party.Party;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PartyRepository extends JpaRepository<Party, Long> {
@@ -15,4 +18,9 @@ public interface PartyRepository extends JpaRepository<Party, Long> {
         WHERE s.stopover_type = 'DESTINATION' AND ST_Distance_Sphere(point(l.longitude, l.latitude), point(:lng, :lat)) <= :radius
         """, nativeQuery = true)
     List<Party> findPartiesWithRadius(@Param("lat") double lat, @Param("lng") double lng, @Param("radius") double radius);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Party p WHERE p.createdAt <= :expiredAt")
+    int deleteExpiredParties(@Param("expiredAt") LocalDateTime expiredAt);
 }
