717a2d1fa3963a9ed6782b499b6eb8a66fd3602a
squareCaaat <danny79052@gmail.com>
2025-05-01 17:19:52 +0900

    feat: TimeZone 설정
    - Datetime의 기본 시간이 서울 표준시로 적용되도록 TimeZone 설정
    #57

--- src/main/java/com/wap/app2/gachitayo/GachitayoApplication.java
+++ src/main/java/com/wap/app2/gachitayo/GachitayoApplication.java
@@ -1,10 +1,17 @@
 package com.wap.app2.gachitayo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GachitayoApplication {
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GachitayoApplication.class, args);
