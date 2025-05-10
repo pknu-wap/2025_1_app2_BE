fcfee2984e3556e4e3d060f31f6fad586c73d5ea
lepitaaar <leekus1005@gmail.com>
2025-05-10 22:52:15 +0900

    refactor: separate review logic #79 review 점수 계산 메서드를 review 서비스로 로직을 분립합니다

--- src/main/java/com/wap/app2/gachitayo/service/member/MemberService.java
+++ src/main/java/com/wap/app2/gachitayo/service/member/MemberService.java
@@ -1,27 +1,23 @@
 package com.wap.app2.gachitayo.service.member;
 
 import com.wap.app2.gachitayo.domain.member.Member;
-import com.wap.app2.gachitayo.domain.member.Review;
 import com.wap.app2.gachitayo.dto.request.EditMemberRequest;
 import com.wap.app2.gachitayo.dto.response.MemberProfileResponseDto;
 import com.wap.app2.gachitayo.error.exception.ErrorCode;
 import com.wap.app2.gachitayo.error.exception.TagogayoException;
 import com.wap.app2.gachitayo.repository.member.MemberRepository;
-import com.wap.app2.gachitayo.repository.member.ReviewRepository;
+import com.wap.app2.gachitayo.service.review.ReviewService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final ReviewRepository reviewRepository;
+    private final ReviewService reviewService;
 
     public ResponseEntity<MemberProfileResponseDto> getMemberProfile(String email) {
         Member member = this.getUserByEmail(email);
@@ -30,7 +26,7 @@ public class MemberService {
             throw new TagogayoException(ErrorCode.MEMBER_NOT_FOUND);
         }
 
-        double review_rate = getReviewRateByMember(member);
+        double review_rate = reviewService.getReviewRateByMember(member.getId());
 
         MemberProfileResponseDto dto = new MemberProfileResponseDto(
                 member.getName(),
@@ -53,7 +49,7 @@ public class MemberService {
             throw new TagogayoException(ErrorCode.MEMBER_NOT_FOUND);
         }
 
-        double review_rate = getReviewRateByMember(member);
+        double review_rate = reviewService.getReviewRateByMember(member.getId());
 
         member.setName(editMemberRequest.name());
 
@@ -68,17 +64,6 @@ public class MemberService {
         );
 
         return ResponseEntity.ok(dto);
-    }
-
-    private double getReviewRateByMember(Member member) {
-        List<Review> reviews = reviewRepository.findAllByMember(member).orElseGet(ArrayList::new);
-
-        double total_score = reviews.stream()
-                            .mapToDouble(Review::getScore)
-                            .average()
-                            .orElse(0.0);
-        //소수 둘째자리까지 반올림
-        return Math.round(total_score * 100) / 100.0;
     }
 
     public Member getUserByEmail(String email) {
