45719c7da9dc43d1d3c7934c7faa4e5f179f7315
squareCaaat <danny79052@gmail.com>
2025-05-13 23:32:05 +0900

    feat: StopoverService `inputBaseFigure` 추가
    - `Stopover` 엔티티에 미터기 요금(기본 요금)을 입력하는 메서드를 서비스단으로 위임
    #53

--- src/main/java/com/wap/app2/gachitayo/service/location/StopoverService.java
+++ src/main/java/com/wap/app2/gachitayo/service/location/StopoverService.java
@@ -1,10 +1,15 @@
 package com.wap.app2.gachitayo.service.location;
 
 import com.wap.app2.gachitayo.domain.location.Stopover;
+import com.wap.app2.gachitayo.dto.request.FareRequestDto;
+import com.wap.app2.gachitayo.error.exception.ErrorCode;
+import com.wap.app2.gachitayo.error.exception.TagogayoException;
 import com.wap.app2.gachitayo.repository.location.StopoverRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -15,5 +20,13 @@ public class StopoverService {
 
     public Stopover saveStopover(Stopover stopover) {
         return stopoverRepository.save(stopover);
+    }
+
+    public void inputBaseFigure(List<FareRequestDto> fareRequestDtoList) {
+        fareRequestDtoList.forEach(fareRequestDto -> {
+            Stopover stopover = stopoverRepository.findById(fareRequestDto.getStopoverId()).orElseThrow(() -> new TagogayoException(ErrorCode.STOPOVER_NOT_FOUND));
+            stopover.getFare().setBaseFigure(fareRequestDto.getFare());
+            stopoverRepository.save(stopover);
+        });
     }
 }
