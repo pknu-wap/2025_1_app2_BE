50132709010db711d7b8b83f02bff81219f91f7d
squareCaaat <danny79052@gmail.com>
2025-05-13 23:35:04 +0900

    feat: StopoverFacade `calculateFinalFigure` 추가
    - 핵심 정산 메커니즘 구현
    - Stopover의 `Fare`의 `baseFigure` 기준으로 정렬
      - 정렬하지 않으면 DESTINATION, STOPOVER 등 금액이 제대로 반영되지 않음
    - 이전 `Stopover`의 `baseFigure` 기억했다가 현재 `Stopover`의 `baseFigure`에서 뺀 값을 정산에 반영
      - 이미 돈을 낸 것이니 뺴주는 것이다.
    - 내린 사람 수 만큼 나눈다.
    - 이때, 나머지가 있을 경우 BOOKKEEPER에게 소액 감면하고 나머지 유저는 소액을 부담한다.
    #53

--- src/main/java/com/wap/app2/gachitayo/service/location/StopoverFacade.java
+++ src/main/java/com/wap/app2/gachitayo/service/location/StopoverFacade.java
@@ -1,16 +1,23 @@
 package com.wap.app2.gachitayo.service.location;
 
+import com.wap.app2.gachitayo.Enum.AdditionalRole;
 import com.wap.app2.gachitayo.Enum.LocationType;
+import com.wap.app2.gachitayo.Enum.PartyMemberRole;
 import com.wap.app2.gachitayo.domain.fare.Fare;
+import com.wap.app2.gachitayo.domain.fare.PaymentStatus;
 import com.wap.app2.gachitayo.domain.location.Location;
 import com.wap.app2.gachitayo.domain.location.Stopover;
 import com.wap.app2.gachitayo.domain.party.Party;
 import com.wap.app2.gachitayo.dto.datadto.LocationDto;
+import com.wap.app2.gachitayo.dto.request.FareRequestDto;
 import com.wap.app2.gachitayo.mapper.LocationMapper;
 import com.wap.app2.gachitayo.service.fare.FareService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -66,4 +73,52 @@ public class StopoverFacade {
         return true;
     }
 
+    public void calculateFinalFare(List<FareRequestDto> fareRequestDtoList, List<Stopover> stopoverList) {
+        stopoverService.inputBaseFigure(fareRequestDtoList);
+
+        List<Stopover> sortedStopoverList = stopoverList.stream()
+                .sorted(Comparator.comparingInt(s -> s.getFare() != null ? s.getFare().getBaseFigure() : 0))
+                .toList();
+
+        int prevBaseFigure = 0;
+        for(Stopover stopover : sortedStopoverList) {
+            if (stopover.getStopoverType().equals(LocationType.START)) continue;
+
+            int baseFare = stopover.getFare().getBaseFigure();
+            List<PaymentStatus> psList = stopover.getPaymentStatusList();
+            int numDropOff = psList.size();
+            if (numDropOff == 0) continue;
+
+            int currentFare = baseFare - prevBaseFigure;
+            int expectFinalFare = currentFare / numDropOff;
+            int remainderFare = currentFare % numDropOff;
+
+            if (remainderFare > 0) {
+                prevBaseFigure = (expectFinalFare + remainderFare) * numDropOff;
+            } else {
+                prevBaseFigure = baseFare;
+            }
+
+            for (PaymentStatus ps : psList) {
+                // 최종 결산자는 항상 목적지에 내린다고 가정. 따라서 목적지에서만 Role를 검사하면 됨
+                if(stopover.getStopoverType().equals(LocationType.DESTINATION)){
+                    PartyMemberRole memberRole = ps.getPartyMember().getMemberRole();
+                    switch (memberRole) {
+                        case HOST:
+                            if (!ps.getPartyMember().getAdditionalRole().equals(AdditionalRole.BOOKKEEPER)) break;
+                        case BOOKKEEPER:
+                            ps.setFinalFigure(expectFinalFare);
+                            ps.setPaid(true);
+                            break;
+                        case MEMBER:
+                            ps.setFinalFigure(expectFinalFare + remainderFare);
+                            break;
+                    }
+                    continue;
+                }
+                ps.setFinalFigure(expectFinalFare + remainderFare);
+            }
+            stopoverService.saveStopover(stopover);
+        }
+    }
 }
