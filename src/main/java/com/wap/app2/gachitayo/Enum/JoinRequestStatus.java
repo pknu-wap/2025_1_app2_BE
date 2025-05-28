c1aa4a6a816d8549e0bce08f08204027df582cc2
squareCaaat <danny79052@gmail.com>
2025-05-28 21:50:57 +0900

    rename: JoinRequestStatus 상태 이름 수정
    #88

--- src/main/java/com/wap/app2/gachitayo/Enum/JoinRequestStatus.java
+++ src/main/java/com/wap/app2/gachitayo/Enum/JoinRequestStatus.java
@@ -1,5 +1,5 @@
 package com.wap.app2.gachitayo.Enum;
 
 public enum JoinRequestStatus {
-    PENDING, ACCEPTED, REJECTED, AUTO_REJECTED, CANCELLED;
+    PENDING, ACCEPTED, REJECTED, AUTO_REJECTED, CANCELED;
 }
