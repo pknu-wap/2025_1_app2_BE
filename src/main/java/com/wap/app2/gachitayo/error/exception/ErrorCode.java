cab6b39f57f7edb2ce891275101e74a6fb1ba2f4
squareCaaat <danny79052@gmail.com>
2025-05-29 01:32:23 +0900

    feat: ErrorCode 에러 코드 추가
    - PathVariable 의 파티 id와 PartyJoinRequest 의 파티 id가 다를 경우 이를 알리는 예외 처리를 위한 코드 추가
    - 그외 파티 참가 요청 에러 코드에 대한 메시지 수정 (의미 명확하게)
    #88

--- src/main/java/com/wap/app2/gachitayo/error/exception/ErrorCode.java
+++ src/main/java/com/wap/app2/gachitayo/error/exception/ErrorCode.java
@@ -39,8 +39,9 @@ public enum ErrorCode {
 
     // JoinRequest
     JOIN_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "JOIN_REQUEST-404-1", "존재하지 않는 파티 요청입니다."),
-    ALREADY_JOIN_REQUEST(HttpStatus.CONFLICT.value(), "JOIN_REQUEST-409-1", "이미 요청한 파티입니다."),
-    ALREADY_REQUEST_HANDLED(HttpStatus.CONFLICT.value(), "JOIN_REQUEST-409-2", "이미 승인된 파티입니다."),
+    ALREADY_JOIN_REQUEST(HttpStatus.CONFLICT.value(), "JOIN_REQUEST-409-1", "이미 참가 요청한 파티입니다."),
+    ALREADY_REQUEST_HANDLED(HttpStatus.CONFLICT.value(), "JOIN_REQUEST-409-2", "이미 요청이 처리된 참가 요청입니다."),
+    JOIN_REQUEST_NOT_MATCH_PARTY(HttpStatus.CONFLICT.value(),  "JOIN_REQUEST-409-3", "참가 요청 파티 id와 참가할 파티 id가 일치하지 않습니다."),
 
     //Stopover
     STOPOVER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "STOPOVER-404-1", "존재하지 않는 경유지입니다."),
