1081c9be2705c73b29f2cdf6b6f8349ebf8104fc
squareCaaat <danny79052@gmail.com>
2025-05-28 15:42:01 +0900

    feat: PartyJoinRequestRepository 메서드 추가
    - 이미 요청한 유저 찾는 메서드
    #88

--- src/main/java/com/wap/app2/gachitayo/repository/party/PartyJoinRequestRepository.java
+++ src/main/java/com/wap/app2/gachitayo/repository/party/PartyJoinRequestRepository.java
@@ -1,7 +1,11 @@
 package com.wap.app2.gachitayo.repository.party;
 
+import com.wap.app2.gachitayo.Enum.JoinRequestStatus;
+import com.wap.app2.gachitayo.domain.member.Member;
+import com.wap.app2.gachitayo.domain.party.Party;
 import com.wap.app2.gachitayo.domain.party.PartyJoinRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PartyJoinRequestRepository extends JpaRepository<Long, PartyJoinRequest> {
+    boolean existsByPartyAndRequesterAndStatus(Party party, Member requester, JoinRequestStatus status);
 }
