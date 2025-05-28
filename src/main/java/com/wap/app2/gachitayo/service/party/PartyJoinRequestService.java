01919ad6df3f441c173b375c28adbb0455f39049
squareCaaat <danny79052@gmail.com>
2025-05-28 16:08:54 +0900

    feat: PartyJoinRequestService `findJoinRequestById` 메서드 추가
    - 단순 CRUD는 서비스단으로 위임
    #88

--- src/main/java/com/wap/app2/gachitayo/service/party/PartyJoinRequestService.java
+++ src/main/java/com/wap/app2/gachitayo/service/party/PartyJoinRequestService.java
@@ -4,6 +4,8 @@ import com.wap.app2.gachitayo.Enum.JoinRequestStatus;
 import com.wap.app2.gachitayo.domain.member.Member;
 import com.wap.app2.gachitayo.domain.party.Party;
 import com.wap.app2.gachitayo.domain.party.PartyJoinRequest;
+import com.wap.app2.gachitayo.error.exception.ErrorCode;
+import com.wap.app2.gachitayo.error.exception.TagogayoException;
 import com.wap.app2.gachitayo.repository.party.PartyJoinRequestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,11 @@ public class PartyJoinRequestService {
     @Transactional(readOnly = true)
     public boolean isAlreadyRequested(Member requester, Party party, JoinRequestStatus status) {
         return partyJoinRequestRepository.existsByPartyAndRequesterAndStatus(party, requester, status);
+    }
+
+    @Transactional(readOnly = true)
+    public PartyJoinRequest findJoinRequestById(Long requestId) {
+        return partyJoinRequestRepository.findById(requestId).orElseThrow(() -> new TagogayoException(ErrorCode.JOIN_REQUEST_NOT_FOUND));
     }
 
     public void requestJoin(Member requester, Party party, JoinRequestStatus status) {
