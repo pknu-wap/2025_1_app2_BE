16c5154498632ee3aaaded3313beb7a75b2e080b
squareCaaat <danny79052@gmail.com>
2025-05-28 15:19:13 +0900

    feat: PartyJoinRequestRepository 추가
    #88

---
+++ src/main/java/com/wap/app2/gachitayo/repository/party/PartyJoinRequestRepository.java
@@ -0,0 +1,7 @@
+package com.wap.app2.gachitayo.repository.party;
+
+import com.wap.app2.gachitayo.domain.party.PartyJoinRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyJoinRequestRepository extends JpaRepository<Long, PartyJoinRequest> {
+}
