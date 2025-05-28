77604ccfe125fde835d01133a222863a8b55ed3c
squareCaaat <danny79052@gmail.com>
2025-05-29 01:13:34 +0900

    feat(Config): WebSocketConfig `customHandshakeHandler` 빈 추가
    - Principal을 명시적으로 등록하기 위해 `DefaultHandshakeHandler`를 커스텀
    - 이메일을 담고 이메일로 식별 가능.
    #88

--- src/main/java/com/wap/app2/gachitayo/config/websocket/WebSocketConfig.java
+++ src/main/java/com/wap/app2/gachitayo/config/websocket/WebSocketConfig.java
@@ -3,11 +3,18 @@ package com.wap.app2.gachitayo.config.websocket;
 import com.wap.app2.gachitayo.jwt.JwtTokenProvider;
 import com.wap.app2.gachitayo.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,8 +27,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .addInterceptors(new WebSocketAuthInterceptor(jwtTokenProvider, memberRepository))
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
+                .setHandshakeHandler(customHandshakeHandler())
                 .withSockJS();
+    }
+
+    @Bean
+    public DefaultHandshakeHandler customHandshakeHandler() {
+        return new DefaultHandshakeHandler() {
+            @Override
+            protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+                String email = (String) attributes.get("memberEmail");
+                return email != null ? new EmailPrincipal(email) : null;
+            }
+        };
     }
 
     @Override
