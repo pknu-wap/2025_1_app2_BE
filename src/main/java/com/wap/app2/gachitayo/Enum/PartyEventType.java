370156ab7c3504c27de76af881320567d22aa32c
squareCaaat <danny79052@gmail.com>
2025-05-29 22:54:49 +0900

    feat: PartyEventType Enum 추가
    - 파티 변경에 대한 유형 이 Enum을 통해 클라이언트의 액션을 결정할 수 있음.
    #88

---
+++ src/main/java/com/wap/app2/gachitayo/Enum/PartyEventType.java
@@ -0,0 +1,14 @@
+package com.wap.app2.gachitayo.Enum;
+
+public enum PartyEventType {
+    // PartyMember
+    MEMBER_JOIN,
+    MEMBER_LEAVE,     // not implementation
+    MEMBER_CANCEL,    // is this type needed?
+    MEMBER_REJECT,    // 2222
+
+    // Party
+    PARTY_CREATE,
+    PARTY_UPDATE,
+    PARTY_DONE;
+}
