39eb827103407572c8d1b41e7c7df04720d1122d
Lepitar <66681282+lepitaaar@users.noreply.github.com>
2025-06-01 16:43:13 +0900

    Revert "feat: 유저 신고기능"

--- src/main/java/com/wap/app2/gachitayo/controller/member/MemberController.java
+++ src/main/java/com/wap/app2/gachitayo/controller/member/MemberController.java
@@ -2,17 +2,15 @@ package com.wap.app2.gachitayo.controller.member;
 
 import com.wap.app2.gachitayo.domain.member.MemberDetails;
 import com.wap.app2.gachitayo.dto.request.EditMemberRequest;
-import com.wap.app2.gachitayo.dto.request.MemberReportRequest;
 import com.wap.app2.gachitayo.dto.response.MemberProfileResponseDto;
 import com.wap.app2.gachitayo.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api")
+@RequestMapping("/api/profile")
 @RequiredArgsConstructor
 @RestController
 @Controller
@@ -21,19 +19,13 @@ public class MemberController {
     private final MemberService memberService;
 
     //추후 member email or id 로 해당유저 정보 가져올 수 있게 리팩토링 예정
-    @GetMapping("/profile")
+    @GetMapping
     public ResponseEntity<MemberProfileResponseDto> getProfile(@AuthenticationPrincipal MemberDetails memberDetails) {
         return memberService.getMemberProfile(memberDetails.getUsername());
     }
 
-    @PatchMapping("/profile")
+    @PatchMapping
     public ResponseEntity<MemberProfileResponseDto> changeName(@AuthenticationPrincipal MemberDetails memberDetails, @RequestBody EditMemberRequest request) {
         return memberService.changeName(memberDetails.getUsername(), request);
-    }
-
-    @PostMapping("/report")
-    public ResponseEntity<?> reportMember(@AuthenticationPrincipal MemberDetails memberDetails,
-                                          @Validated @RequestBody MemberReportRequest request) {
-        return memberService.reportMember(memberDetails.getUsername(), request);
     }
 }
